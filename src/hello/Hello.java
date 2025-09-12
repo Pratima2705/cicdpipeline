001432e1aca828ef784483e91aa83294c37ea709
Pratima2705 <151670110+Pratima2705@users.noreply.github.com>
2025-09-12 14:01:06 +0530

    Add error message to Hello.java output

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("checkinnnng webhooksss");
+    System.out.println("404 error due to post");
   }
 }
